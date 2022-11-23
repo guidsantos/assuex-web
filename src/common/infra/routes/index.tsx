95f35ea0f02c3c0434086d10fbcc2178ef49c1e4
pedrolnamk <pedrolna18@gmail.com>
2022-11-23 20:49:01 -0300

    Linhas

--- src/common/infra/routes/index.tsx
+++ src/common/infra/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../../../domain/Home/home.page";
+import Linhas from "../../../domain/Linhas/pages/linhas.page";
 import SignIn from "../../../domain/Login/SignIn/pages/signin.page";
 import SignUp from "../../../domain/Login/SignUp/pages/signup.page";
 import { Route as AuthRoute } from "./Router.component";
@@ -10,6 +11,7 @@ export const RoutesFC: React.FC = () => {
     <Routes>
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/linhas" element={<Linhas />} />
       <Route
         path="/home"
         element={<AuthRoute isPrivate={true} outlet={<Home />} />}
---
+++ src/domain/Linhas/pages/linhas.page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Content } from "./linhas.styles";
+const Linhas: React.FC =() => {
+    return <Content>
+        <h1>  Ola  </h1>
+
+    </Content>
+}
+export default Linhas
